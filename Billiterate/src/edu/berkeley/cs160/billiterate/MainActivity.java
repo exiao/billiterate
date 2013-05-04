f80de6b27c071521d4ec97951761878980cdcf49
Linda <lindacai@noemail.com>
2013-05-04 14:04:41 -0700

    cleanup

--- Billiterate/src/edu/berkeley/cs160/billiterate/MainActivity.java
+++ Billiterate/src/edu/berkeley/cs160/billiterate/MainActivity.java
@@ -36,7 +36,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
-import android.view.ViewParent;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
